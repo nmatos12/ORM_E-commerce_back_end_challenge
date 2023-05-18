17b8781d1f6b383ff0d395fcf20dad4e1f9d28cd
unknown <nataliematos12@gmail.com>
2023-05-18 03:57:16 -0400

    defined columns in models and created foreign keys

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product.js');
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category.js');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,24 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+  },
+  
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
   },
   {
     sequelize,
